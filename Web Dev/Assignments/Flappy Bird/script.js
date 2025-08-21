db000d4220e9fb853e28274ce896e6e09f18859f
Warrior07A <akshatmittalofficial@gmail.com>
2025-08-21 15:56:04 +0000

    alert not added

---
+++ Web Dev/Assignments/Flappy Bird/script.js
@@ -0,0 +1,11 @@
+
+window.onload=function(){
+    var board=document.getElementById("canvas");
+    board.width=board.style.width="100";
+    board.heigth=board.style.height="100";
+    context=board.getContext("2d");                                     //used for drawing on the canvas
+
+    context.fillStyle="green";
+    context.fillRect(bird.)
+
+}
---
+++ Web Dev/Class 4 JS Begins/facebook landing page/readme.md
@@ -0,0 +1,2 @@
+##div>.children 
+press enter bro
